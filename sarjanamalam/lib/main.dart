dbad32ad393cd18d087e24bcf10d07ac14422515
Star CodeX <hello@bintangtobing.com>
2020-04-01 14:48:16 +0700

    update

---
+++ sarjanamalam/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sarjanamalam',
+      theme: ThemeData(
+        primarySwatch: Colors.white,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(children: <Widget>[
+          new Image.asset("images/logo.png"),
+          new Image.asset("img/google-sign-in.png"),
+        ],)
+      )
+    );
+  }
+}
